7fc46f6592d3518a72654eb0fbb9454daec83629
Edward Vuong <ed@edwardvuong.com>
2019-02-27 12:04:11 -0800

    Yes

---
+++ Assets/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private bool hasEnded = false;
+    public float restartDelay = 1f;
+    
+    public void EndGame()
+    {
+        if (hasEnded == false)
+        {
+            hasEnded = true;
+            Invoke("Restart", restartDelay);
+        }
+
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Temple Run 
+It's temple run but there's no temple and you can't run you can only slide.</br>
+
+Implemented using this [this tutorial](https://www.youtube.com/watch?v=j48LtUkZRjU&list=PLPV2KyIb3jR53Jce9hP7G5xC4O9AgnOuL&index=1)
+ by Brackys. 
